f97013b5492796b5ee27abad1057d2cc7c51f6c2
Alexander <alexander.lindahl@forefront.se>
2021-09-22 17:46:53 +0200

    remove code and tests

--- src/rules/AtLeastThreeDsRule.ts
+++
@@ -1,7 +0,0 @@
-import { IRule } from "./IRule";
-
-export class AtLeastThreeDsRule implements IRule {
-  isNice = (input: string) => {
-    return input.split("").filter((c) => c.toLowerCase() === "d").length >= 3;
-  };
-}

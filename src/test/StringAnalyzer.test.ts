4022b58f599d98dc9bcc15ea1c90904da87d212a
Alexander <alexander.lindahl@forefront.se>
2021-09-23 12:43:19 +0200

    update string analyzer test

--- src/test/StringAnalyzer.test.ts
+++ src/test/StringAnalyzer.test.ts
@@ -1,12 +1,20 @@
+import { AtLeastThreeDsRule } from "../rules/AtLeastThreeDsRule";
 import { AtLeastThreeVowelsRule } from "../rules/AtLeastThreeVowelsRule";
 import { IRule } from "../rules/IRule";
+import { LetterTwiceInARowRule } from "../rules/LetterTwiceInARowRule";
+import { NoBadCombinationRule } from "../rules/NoBadCombinationRule";
 import { StringAnalyzer } from "../StringAnalyzer/StringAnalyzer";
 
 describe("Make sure a string does not conatain a bad combination", () => {
   it("Should make sure a string does not contain a bad combination", () => {
-    const rules: IRule[] = [new AtLeastThreeVowelsRule()];
+    const rules: IRule[] = [
+      new AtLeastThreeVowelsRule(),
+      new AtLeastThreeDsRule(),
+      new NoBadCombinationRule(["pq", "xy", "ab", "cd"]),
+      new LetterTwiceInARowRule(),
+    ];
     const analyzer = new StringAnalyzer(rules);
-    const niceRes = analyzer.analyze("harbcee");
+    const niceRes = analyzer.analyze("harbceddde");
 
     expect(niceRes).toBe(true);
 
